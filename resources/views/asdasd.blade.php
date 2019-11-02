3fff2c9df11e8e46b8826f009398465febc324be
hassaneit8 <hassaneit8@gmail.com>
2019-11-02 03:48:22 +0200

    test

---
+++ resources/views/asdasd.blade.php
@@ -0,0 +1,24 @@
+<div class="links">
+    <a href="https://laravel.com/docs">ddddddd</a>
+    <a href="https://laracasts.com">Laracasts</a>
+    <a href="https://laravel-news.com">dsdadasd</a>
+    <a href="https://blog.laravel.com">Blog</a>
+    <a href="https://nova.laravel.com">Nova</a>
+    <a href="https://forge.laravel.com">Forge</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <a href="https://blog.laravel.com">Blog</a>
+    <a href="https://nova.laravel.com">Nova</a>
+    <a href="https://forge.laravel.com">Forge</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <a href="https://blog.laravel.com">Blog</a>
+    <a href="https://nova.laravel.com">Nova</a>
+    <a href="https://forge.laravel.com">Forge</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <a href="https://blog.laravel.com">Blog</a>
+    <a href="https://nova.laravel.com">Nova</a>
+    <a href="https://forge.laravel.com">Forge</a>
+    <a href="https://github.com/laravel/laravel">GitHub</a>
+</div>
+</div>
